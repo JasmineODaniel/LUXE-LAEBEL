6a245137f4004fdfe53f11f7a2c18dedcb59fee6
THENEVERDONE <163352801+JasmineODaniel@users.noreply.github.com>
2026-01-13 09:02:39 -0800

    Add files via upload

---
+++ ProductsPage.tsx
@@ -0,0 +1,11 @@
+// src/pages/ProductsPage.tsx
+import React from "react";
+
+export default function ProductsPage() {
+  return (
+    <div>
+      <h1>All Products</h1>
+      {/* You can render your products grid here */}
+    </div>
+  );
+}
